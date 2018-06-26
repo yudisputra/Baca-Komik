dd195163258219a264dbdbe1c0d692129e14b76f
Yudistira Putra <putra.nijikun@gmail.com>
2018-06-26 12:01:42 +0700

    Footer dan Header

---
+++ application/views/homepage.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('layout/base_start');?>
+
+
+
+<?php $this->load->view('layout/base_end');?>
---
+++ application/views/layout/base_end.php
@@ -0,0 +1,10 @@
+
+
+		<!-- jQuery -->
+		<script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js') ?>"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>

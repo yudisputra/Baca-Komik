7dbd7361e593bbd32941ea836db93066674419c4
Refly Ilham Syabana <35444939+reflyilham@users.noreply.github.com>
2018-06-26 14:06:49 +0700

    Ganti base start , homepage ,header

--- application/views/layout/base_start.php
+++ application/views/layout/base_start.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Title Page</title>
+		<title>Baca Komik</title>
 
 		<!-- Bootstrap CSS -->
 		<link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
@@ -15,6 +15,11 @@
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		<style type="text/css">
+			body {
+				font-family: 'Segoe UI';
+			}
+		</style>
 	</head>
 	<body>
 	

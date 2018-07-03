84a07a639bd182fb1a35d356790e084877d72b21
Refly Ilham Syabana <35444939+reflyilham@users.noreply.github.com>
2018-07-03 09:28:16 +0700

    Chapter Terbaru + Manga Terlaris
    nambah View

---
+++ application/views/chapterTerbaru.php
@@ -0,0 +1,141 @@
+<?php $this->load->view('layout/base_start'); ?>
+
+<div class="container-fluid">
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Baca Komik Online </h1>
+			<p>Website Manga - Anime | Terlengkap dan terpercaya</p>
+			<p>
+				<a class="btn btn-primary btn-lg">Learn more</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+    <div class="container-fluid">
+            <div class="row">
+            <div class="panel panel-success">
+            <div class="panel-heading"><span class="glyphicon glyphicon-book"></span> Chapter Terbaru </div>
+            <div class="panel-body">
+
+      <table id="example" class="table table-responsive table-hover">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Chapter</th>
+                <th>Release date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Ashton Cox</td>
+                <td>Junior Technical Author</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Brielle Williamson</td>
+                <td>Integration Specialist</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Herrod Chandler</td>
+                <td>Sales Assistant</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Rhona Davidson</td>
+                <td>Integration Specialist</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Colleen Hurst</td>
+                <td>Javascript Developer</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Sonya Frost</td>
+                <td>Software Engineer</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Jena Gaines</td>
+                <td>Office Manager</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Quinn Flynn</td>
+                <td>Support Lead</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Charde Marshall</td>
+                <td>Regional Director</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Haley Kennedy</td>
+                <td>Senior Marketing Designer</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Tatyana Fitzpatrick</td>
+                <td>Regional Director</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Michael Silva</td>
+                <td>Marketing Designer</td>
+                <td>London</td>
+            </tr>
+            <tr>
+                <td>Paul Byrd</td>
+                <td>Chief Financial Officer (CFO)</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Gloria Little</td>
+                <td>Systems Administrator</td>
+                <td>2008/11/28</td>
+            </tr>
+            <tr>
+                <td>Dai Rios</td>
+                <td>Personnel Lead</td>
+                <td>2008/11/28</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Chapter</th>
+                <th>Release date</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<?php $this->load->view('layout/footer');
---
+++ application/views/mangaTerlaris.php
@@ -0,0 +1,117 @@
+<?php $this->load->view('layout/base_start'); ?>
+
+<div class="container-fluid">
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Baca Komik Online </h1>
+			<p>Website Manga - Anime | Terlengkap dan terpercaya</p>
+			<p>
+				<a class="btn btn-primary btn-lg">Learn more</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="container-fluid">
+		<div class="row">
+		<div class="panel panel-success">
+		<div class="panel-heading"><span class="glyphicon glyphicon-book"></span> Manga Terlaris</div>
+		<div class="panel-body">
+		    <form class="form-group has-feedback">
+		         <label class="control-label sr-only" for="inputSuccess5">Hidden label</label>
+		         <input type="text" class="form-control" id="inputSuccess5" placeholder="Search your manga ...">
+		         <span class="glyphicon glyphicon-search form-control-feedback"></span>
+		     </form>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+							
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+			<br>
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<div class="col-xs-6 col-sm-3">
+				<div id="hover-cap-4col">
+						<div class="caption">
+							<center><img src="<?php echo base_url('assets/images/thumb_cover.jpg') ?>" class="img-responsive center"></center>
+							<p align="center">Tsuki ga michibiku isekai douchuu</p>
+						</div>
+				</div>
+			</div>
+
+			<nav aria-label="...">
+			  <ul class="pagination">
+			    <li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+			    <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+			    <li><a href="#">2 <span class="sr-only">(current)</span></a></li>
+			    <li><a href="#">3 <span class="sr-only">(current)</span></a></li>
+			    <li><a href="#">4 <span class="sr-only">(current)</span></a></li>
+			    <li><a href="#">5 <span class="sr-only">(current)</span></a></li>
+			    <li class="disabled"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></
+			  </ul>
+			</nav>
+	</div>
+</div>
+</div>
+</div>
+</div>
+
+<?php $this->load->view('layout/footer'); ?> 

6aa9a918c498957b17c6287091d5e1eb99154ff1
Yudistira Putra <putra.nijikun@gmail.com>
2018-07-03 20:54:45 +0700

    Pembetulan

--- application/views/admin/admin_komik.php
+++ application/views/admin/admin_komik.php
@@ -1,5 +1,8 @@
 <?php $this->load->view('admin/admin_base_start');?>
 
-    <div id="jsGrid"></div>
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		 <h1>Admin Komik</h1>
+		 <div id="jsGrid"></div>
+	</div>
 
 <?php $this->load->view('admin/admin_base_end');?>

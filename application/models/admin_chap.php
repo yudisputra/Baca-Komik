80cd857bed5209bebab5029674ebd0943a459fc5
Yudistira Putra <putra.nijikun@gmail.com>
2018-07-17 21:50:43 +0700

    Multi Upload Selesai
    Liat browser di bookmark untuk prefensi

--- application/models/admin_chap.php
+++ application/models/admin_chap.php
@@ -31,4 +31,18 @@ class admin_chap extends CI_Model {
         $this->db->where('idchapter', $id); 
         $this->db->delete('chapter');
     }
+
+    public function insertchapter()
+    {
+         $object = array('idkomik' => $this->input->post('idkomik'), 'namakomik' => $this->input->post('namakomik'), 'pengarang' => $this->input->post('pengarang'),'status' => $this->input->post('status'),'ringkasan' => $this->input->post('ringkasan'),'cover' => $this->upload->data('file_name'));
+         $this->db->insert('komik', $object);
+    }
+
+    public function insertchapterbaru()
+    {
+        $data = $this->upload->get_multi_upload_data('file_name');   
+        $jumlah = count($data);
+         $object = array('fk_idkomik' => $this->input->post('fk_idkomik'), 'volchapter' => $this->input->post('volchapter'), 'judulchapter' => $this->input->post('judulchapter'),'tanggalrilis' => $this->input->post('tanggalrilis'),'jum' => $jumlah);
+         $this->db->insert('chapter', $object);
+    }
 }
---
+++ application/views/admin/admin_tambahchapter.php
@@ -0,0 +1,39 @@
+<?php $this->load->view('admin/admin_base_start');?>
+
+	<div class="container-fluid">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<?php echo form_open_multipart('Admin_chapter/insertchapter'); ?>
+
+			<legend>Tambah Chapter Komik</legend>
+			<?php echo validation_errors(); ?>
+			<div class="form-group">
+			<label for="sel1">Pilih Judul Komik</label>
+  			<select class="form-control" id="judulkomik" name="judulkomik">
+  			<?php foreach ($judulkomik as $key) { ;?>
+   		    <option value="<?php echo $key->idkomik?>" name="fk_idkomik"><?php echo $key->namakomik?></option>
+   		     <?php } ?> 
+  			</select>
+  			<br>
+			<label for="">Volume Chapter</label>
+			<input type="text" class="form-control" id="volchapter" name="volchapter">
+			<br>
+			<label for="">Judul Chapter</label>
+			<input type="text" class="form-control" id="judulchapter" name="judulchapter">
+			<br>
+			<label for="">Tanggal Rilis</label>
+			<input type="date" class="form-control" id="tanggalrilis" name="tanggalrilis">
+			<br>
+			<div class="form-group">
+				<label for="">Cover Komik</label>
+				<input type="file" name="files[]" multiple />
+			</div>
+
+			</div>
+			<br>
+			<button type="submit" class="btn btn-primary">Submit</button>
+			<?php echo form_close(); ?>
+	</div>	 
+	</div>
+
+<?php $this->load->view('admin/admin_base_end');?>
